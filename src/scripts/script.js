ec1ab17a3770dbd643080033da70239917b2f5f0
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2018-05-23 10:52:57 +0700

    finish html, css

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,41 @@
 $(document).ready(function(){
-    $('.multiple-items').slick({
+   $('.multiple-items').slick({
 	  infinite: true,
 	  slidesToShow: 4,
 	  slidesToScroll: 4,
-	  arrows: false
+	  arrows: false,
+	  autoplay: true,
+  	autoplaySpeed: 2000,
 	});
-	$('.left').click(function(){
+	$('.team-member .left').click(function(){
 		$('.multiple-items').slick('slickPrev');
 	});
-	$('.right').click(function(){
+	$('.team-member .right').click(function(){
 		$('.multiple-items').slick('slickNext');
+	});
+	$('.quote-slide').slick({
+		arrows: false,
+		autoplay: true,
+  	autoplaySpeed: 2000,
+	});
+	$('.quote .left').click(function(){
+		$('.quote-slide').slick('slickPrev');
+	});
+	$('.quote .right').click(function(){
+		$('.quote-slide').slick('slickNext');
+	});
+	$('.brand-slide').slick({
+	  infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 4,
+	  arrows: false,
+	  autoplay: true,
+  	autoplaySpeed: 2000,
+	});
+	$('.brand .left').click(function(){
+		$('.brand-slide').slick('slickPrev');
+	});
+	$('.brand .right').click(function(){
+		$('.brand-slide').slick('slickNext');
 	});
 });
